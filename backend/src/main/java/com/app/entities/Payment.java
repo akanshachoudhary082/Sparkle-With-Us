b27e1b171d8dd1d8dd4d33670524fc24e13ba0d1
unknown <Akansha Choudhary>
2024-08-08 11:28:07 +0530

    payment

--- backend/src/main/java/com/app/entities/Payment.java
+++ backend/src/main/java/com/app/entities/Payment.java
@@ -1,5 +1,8 @@
 package com.app.entities;
 
+import java.time.LocalDate;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
@@ -7,5 +10,80 @@ import javax.persistence.Table;
 @Table(name="payment")
 public class Payment extends BaseEntity
 {
+
+	private Long transactionId;
+	private Long bookingId;
+	private Double amount;
+	private LocalDate paymnetDate;
+	private PaymentMode paymentMode;
+	@Column(length = 20)
+	private PaymentStatus paymentStatus;
+	
+	
+	public Payment() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public Payment(Long id) {
+		super(id);
+		// TODO Auto-generated constructor stub
+	}
+	public Payment(Long transactionId, Long bookingId, Double amount, LocalDate paymnetDate, PaymentMode paymentMode,
+			PaymentStatus paymentStatus) {
+		super();
+		this.transactionId = transactionId;
+		this.bookingId = bookingId;
+		this.amount = amount;
+		this.paymnetDate = paymnetDate;
+		this.paymentMode = paymentMode;
+		this.paymentStatus = paymentStatus;
+	}
+	
+	
+	public Long getTransactionId() {
+		return transactionId;
+	}
+	public void setTransactionId(Long transactionId) {
+		this.transactionId = transactionId;
+	}
+	public Long getBookingId() {
+		return bookingId;
+	}
+	public void setBookingId(Long bookingId) {
+		this.bookingId = bookingId;
+	}
+	public Double getAmount() {
+		return amount;
+	}
+	public void setAmount(Double amount) {
+		this.amount = amount;
+	}
+	public LocalDate getPaymnetDate() {
+		return paymnetDate;
+	}
+	public void setPaymnetDate(LocalDate paymnetDate) {
+		this.paymnetDate = paymnetDate;
+	}
+	public PaymentMode getPaymentMode() {
+		return paymentMode;
+	}
+	public void setPaymentMode(PaymentMode paymentMode) {
+		this.paymentMode = paymentMode;
+	}
+	public PaymentStatus getPaymentStatus() {
+		return paymentStatus;
+	}
+	public void setPaymentStatus(PaymentStatus paymentStatus) {
+		this.paymentStatus = paymentStatus;
+	}
+	
+	@Override
+	public String toString() {
+		return "Payment [transactionId=" + transactionId + ", bookingId=" + bookingId + ", amount=" + amount
+				+ ", paymnetDate=" + paymnetDate + ", paymentMode=" + paymentMode + ", paymentStatus=" + paymentStatus
+				+ "]";
+	}
+	
+	
 	
 }
---
+++ backend/src/main/java/com/app/entities/PaymentStatus.java
@@ -0,0 +1,6 @@
+package com.app.entities;
+
+public enum PaymentStatus {
+
+	CONFIRMED,CANCELLED;
+}
